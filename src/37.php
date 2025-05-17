f3aed49f3a2971f57d4601758f9ade5fd1e9e055
dalieyackivah <dalieyackivah@gmail.com>
2025-05-17 08:06:15 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,4 @@
+<?php
+// This PHP code is used to demonstrate the use of arrays and basic string operations.
+$colors = array("red", "blue", "green", "yellow");
+echo "Colors: " . implode(", ", $colors) . "\n";
